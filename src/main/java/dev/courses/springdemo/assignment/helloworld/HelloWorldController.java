82f4824a8ad3431ccdc88afd144d3aebc395c151
DavLac <david.lacoste2@gmail.com>
2022-12-13 01:57:03 +0100

    Hello world controller

---
+++ src/main/java/dev/courses/springdemo/assignment/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package dev.courses.springdemo.assignment.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/welcome")
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello world";
+    }
+}
