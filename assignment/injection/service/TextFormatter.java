2f153cbe0f17223b25fb32a3471e43529aadc015
DavLac <david.lacoste2@gmail.com>
2022-12-15 10:50:34 +0100

    Update

--- assignment/injection/service/TextFormatter.java
+++ assignment/injection/service/TextFormatter.java
@@ -1,12 +1,14 @@
 
 public class TextFormatter {
 
-    // inject DateTimeService dependency
+    private final DateTimeService dateTimeService;
+
+    public TextFormatter(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
 
     public String addDateTime(String input) {
-        // Should return concatenation of input + time
-        // return input + " - " + DateTimeService.getDateTimeNow();
-        return input;
+        return input + " - " + dateTimeService.getDateTimeNow();
     }
 
 }
